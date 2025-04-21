68548c8a370a54b2b8e1b49dbf06e0d7c02bc944
Jeff Dickerson <115676609+jeff-dickerson@users.noreply.github.com>
2025-04-21 18:13:52 -0500

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -35,7 +35,7 @@ const About = () => {
               <div>
                 <h2 className="text-2xl font-bold mb-4">Hi, I'm Jeff!</h2>
                 <p className="text-gray-600 mb-6">
-                  I'm an AI architect, which is just a fancy way of saying I build smart tools and systems that solve real-world problems using artificial intelligence. I'm the founder of Kontrak IQ, an enterprise contract data management platform powered by a vertical AI Agent. My expertise sits at the intersection of systems and software development.
+                  I'm an AI architect, which is just a fancy way of saying I design and build complex systems that help people solve real-world problems using artificial intelligence. I'm the founder of Kontrak IQ, an enterprise contract data management platform powered by a vertical AI Agent. My expertise sits at the intersection of systems and software development.
                 </p>
                 <p className="text-gray-600 mb-6">
                   I focus on making AI less intimidating and more meaningful. Whether it's designing products, creating strategies, or just sharing insights, my goal is to bridge the gap between humans and advanced tech in a way that feels purposeful.
@@ -47,7 +47,7 @@ const About = () => {
                   I love experimenting with and contributing to blockchain and crypto projects, having led a native build and published empirical research on several ecosystems. I'm passionate about how technology shapes our lives and use my expertise to help organizations of all sizes harness AI to make smarter decisions, streamline workflows, and discover untapped opportunities.
                 </p>
                 <p className="text-gray-600">
-                  Every two weeks, I publish G-Factor, where I dive into ideas about intelligence—how we think, how machines think, and how the two intersect. I believe technology should work for us—not the other way around. And I'm here to help make that happen.
+                  Every two weeks, I publish g-Factor, where I dive into ideas about intelligence—how we think, how machines think, and how the two intersect. I believe technology should work for us—not the other way around. And I'm here to help make that happen.
                 </p>
               </div>
             </div>
